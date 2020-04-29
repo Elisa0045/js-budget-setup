392e5f0613980eb6de8d5e18d479b77172de78f3
Elisa <elisacastaneda0045@gmail.com>
2020-04-29 22:40:22 +0200

    expense form
    i wrote t the code for add an expense to the application, and the development strategy

--- js/handlers.js
+++ js/handlers.js
@@ -9,5 +9,19 @@ var handler = {
       handler: "budgetForm",
       object: JSON.parse(JSON.stringify(this.ui)),
     });
+  }, //expense method
+  expenseForm() {
+    event.preventDefault();
+    this.ui.submitExpenseForm();
+    log.push({
+      handler: "expenseForm",
+      object: JSON.parse(JSON.stringify(this.ui)),
+    });
+  }, //expense list
+  expenseList() {
+    log.push({
+      handler: "expenseList",
+      object: JSON.parse(JSON.stringify(this.ui)),
+    });
   },
 };
